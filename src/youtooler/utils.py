a3acf3621e6415944f8a18cdc10c120ab352e565
Fraccs <aliprandifrancescopp@gmail.com>
2022-07-04 17:17:11 +0200

    Removed UserWarings from bs4

--- src/youtooler/utils.py
+++ src/youtooler/utils.py
@@ -1,7 +1,7 @@
 import isodate
 import requests
 from argparse import ArgumentParser
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, GuessedAtParserWarning
 from colorama import Fore, Style
 from sys import stderr
 
@@ -44,7 +44,7 @@ def get_video_duration(url: str) -> int:
             continue
 
         # Parsing response
-        parsed_html = BeautifulSoup(markup=html.text)
+        parsed_html = BeautifulSoup(markup=html.text, features='html5lib')
 
         # Searching for the tag <meta itemprop="duration" content="">
         duration_tag = parsed_html.find('meta', {'itemprop': 'duration'})
@@ -78,7 +78,7 @@ def verify_youtube_url(url: str) -> bool:
         except ConnectionError:
             continue
 
-        parsed_html = BeautifulSoup(markup=html.text)
+        parsed_html = BeautifulSoup(markup=html.text, features='html5lib')
 
         # Searching for the tag <meta name="title" content="">
         title_tag = parsed_html.find('meta', {'name': 'title'})
