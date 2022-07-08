fc7269351d0b8f04fb7fdc70dc20029a1e2a3d17
Fraccs <aliprandifrancescopp@gmail.com>
2022-07-08 10:09:05 +0200

    test(*): add placeholder test
    Replaced test_storage_dir test with a placeholder test

--- tests/test_youtooler.py
+++ tests/test_youtooler.py
@@ -2,11 +2,5 @@ import os
 from youtooler.app import *
 
 class TestYoutooler:
-    def test_storage_dir(self):
-        app = Youtooler()
-
-        assert os.path.isdir('/tmp/youtooler') == True
-
-        app.__clean__()
-
-        assert os.path.isdir('/tmp/youtooler') == False
+    def test_placeholder(self):
+        assert 1 == 1
